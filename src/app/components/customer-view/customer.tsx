f1d3c52ee5916474f297888198e619d26ae4725b
tomasBONILL <tomasedubonilla@gmail.com>
2025-05-26 09:30:45 -0600

    Avance

---
+++ src/app/components/customer-view/customer.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Customer() {
+  return (
+    <div className="flex">
+        <div className="md-w-1/2 space-y-4">
+            <h2 className="text-x1 font-bold">Customer</h2>
+            <p className="text-gray-700"></p>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Main2 from "./components/main-view/main";
+import Customer from "./components/customer-view/customer";
 
 export default function Home() {
   return (
     
     <div>
       
-      <Main2 />
+      <Customer/>
      
     </div>
   );
