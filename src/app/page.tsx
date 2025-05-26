7460933420b807f5fe751bafe9e8615ea4396368
Chris85234 <christianalfaro530@gmail.com>
2025-05-26 09:23:04 -0600

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Main2 from "./components/main-view/main";
+import Login from "./components/login/login";
 
 export default function Home() {
   return (
     
     <div>
       
-      <Main2 />
+     <Login/>
      
     </div>
   );
