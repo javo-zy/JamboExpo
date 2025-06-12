83f88efabdaffb27a7d94269199fb4f3f4f24e74
LAPT-CSH-59 <natalia.pineda2024@hilasal.superate.org.sv>
2025-06-12 09:27:08 -0600

    Final location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Button from '@/components/Button';
 import Image from 'next/image';
-import Link from 'next/link'; 
-
+import Link from 'next/link';
+import React from 'react'
 export default function Home() {
   return (
-  <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
 
       <section className="relative w-full h-[60vh] md:h-[70vh] flex items-center justify-center text-center text-white overflow-hidden p-4">
         <Image
@@ -150,7 +150,7 @@ export default function Home() {
         </Link>
       </section>
     </div>
-    
+
   );
 
 
--- src/app/search-location/page.tsx
+++ src/app/search-location/page.tsx
@@ -1,17 +1,11 @@
+import MapShow from '@/components/map.js';
 
-import type { NextPage } from 'next';
-import React from 'react';
-
-const Home: NextPage = () => {
+export default function SearchLocationPage() {
     return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">Search Location</h1>
-            <p className="mb-4">This page is under construction. Please check back later!</p>
-            <div className="bg-gray-100 p-4 rounded-lg">
-                <p className="text-gray-600">We are working hard to bring you this feature. Stay tuned!</p>
-            </div>
-    </div>
-);
-};
-
-export default Home;
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-3xl font-bold mb-4">Search ubication</h1>
+            <p className="text-lg text-gray-700 mb-8">Select an ubication in the map to continue.</p>
+            <MapShow />
+        </div>
+    );
+}
