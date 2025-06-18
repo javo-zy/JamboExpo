9dc2238f5c2a1e51c64ecbd1d8bb89dd9bdc7bc1
global <jefferson.escobar2024@hilasal.superate.org.sv>
2025-06-18 09:22:54 -0600

    repair

--- src/app/search-location/page.tsx
+++ src/app/search-location/page.tsx
@@ -3,8 +3,8 @@ import MapShow from '@/components/map.js';
 export default function SearchLocationPage() {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-3xl text-black font-bold mb-4">Search ubication</h1>
-            <p className="text-lg text-gray-700 mb-8">Select an ubication in the map to continue.</p>
+            <h1 className="text-3xl text-black font-bold mb-4">Busca una dirección</h1>
+            <p className="text-lg text-gray-700 mb-8">Selecciona una dirección para comenzar</p>
             <MapShow />
         </div>
     );
