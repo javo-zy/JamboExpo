d61479f025791232f116ab975ffcb32da6c5a635
LAPT-CSH-16 <tomasedubonilla@gmail.com>
2025-06-18 08:30:19 -0600

    repair

--- src/app/search-location/page.tsx
+++ src/app/search-location/page.tsx
@@ -3,7 +3,7 @@ import MapShow from '@/components/map.js';
 export default function SearchLocationPage() {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-3xl font-bold mb-4">Search ubication</h1>
+            <h1 className="text-3xl text-black font-bold mb-4">Search ubication</h1>
             <p className="text-lg text-gray-700 mb-8">Select an ubication in the map to continue.</p>
             <MapShow />
         </div>
